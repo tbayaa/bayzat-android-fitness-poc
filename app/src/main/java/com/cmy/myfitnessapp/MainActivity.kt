0052c5b7c27a1049cce5bd65fea32d1ae1b55366
cmyildiz <can@bayzat.com>
2022-03-16 18:26:20 +0300

    Refactor code

--- app/src/main/java/com/cmy/myfitnessapp/MainActivity.kt
+++ app/src/main/java/com/cmy/myfitnessapp/MainActivity.kt
@@ -4,9 +4,9 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.cmy.myfitnessapp.PermissionConstants.GOOGLE_FIT_PERMISSIONS_REQUEST_CODE
+import com.cmy.myfitnessapp.PermissionConstants.HISTORY_PERMISSIONS_REQUEST_CODE
+import com.cmy.myfitnessapp.PermissionConstants.SESSION_PERMISSIONS_REQUEST_CODE
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.fitness.Fitness
 import com.google.android.gms.fitness.FitnessOptions
@@ -24,91 +24,48 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        requestSessionAuth()
+        getSessionHeartRateInformation()
+        getDailyStepInformation()
     }
 
-    private fun requestSessionAuth() {
+    private fun getSessionHeartRateInformation() {
         val dataTypes =
             arrayListOf(
                 DataType.TYPE_HEART_RATE_BPM,
-                DataType.AGGREGATE_HEART_RATE_SUMMARY,
-                DataType.AGGREGATE_STEP_COUNT_DELTA,
-                DataType.TYPE_STEP_COUNT_DELTA,
             )
+        val arePermissionsGranted = areSessionPermissionsGranted(dataTypes)
 
-        val sessionFitnessOptions = getSessionFitnessOptions(dataTypes)
-
-        val googleSignInAccount =
-            GoogleSignIn.getAccountForExtension(this, sessionFitnessOptions)
-
-        val isGranted =
-            GoogleSignIn.hasPermissions(googleSignInAccount, sessionFitnessOptions)
-
-        if (!isGranted) {
-            GoogleSignIn.requestPermissions(
-                this,
-                GOOGLE_FIT_PERMISSIONS_REQUEST_CODE,
-                googleSignInAccount,
-                sessionFitnessOptions
-            )
+        if (arePermissionsGranted) {
+            getSessionHeartRateAverage()
         } else {
-            sendSessionsRequestAndGetResponse()
+            requestSessionPermissions(dataTypes)
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        when (resultCode) {
-            Activity.RESULT_OK -> when (requestCode) {
-                GOOGLE_FIT_PERMISSIONS_REQUEST_CODE -> sendAndGetResponse()
-                else -> {
-                    // Result wasn't from Google Fit
-                }
-            }
-            else -> {
-                // Permission not granted
-            }
-        }
+    private fun areSessionPermissionsGranted(dataTypesOfPermissions: List<DataType>): Boolean {
+        val fitnessOptions = getSessionFitnessOptions(dataTypesOfPermissions)
+        val googleAccount = GoogleSignIn.getAccountForExtension(this, fitnessOptions)
+        return GoogleSignIn.hasPermissions(googleAccount, fitnessOptions)
     }
 
-    private fun sendAndGetResponse() {
-        val dataTypes =
-            arrayListOf(DataType.TYPE_STEP_COUNT_DELTA)
+    private fun requestSessionPermissions(dataTypesOfPermissions: List<DataType>) {
+        val fitnessOptions = getSessionFitnessOptions(dataTypesOfPermissions)
+        val googleAccount =
+            GoogleSignIn.getAccountForExtension(this, fitnessOptions)
 
-        val sessionFitnessOptions = getHistoryFitnessOptions(dataTypes)
-
-        val startTime = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            LocalDate.now().atStartOfDay(ZoneId.systemDefault()).minusDays(10)
-        } else {
-            TODO("VERSION.SDK_INT < O")
-        }
-        val endTime = LocalDateTime.now().atZone(ZoneId.systemDefault())
-
-        val sessionReadRequest = getHistoryReadRequest(
-            startTime = startTime.toEpochSecond(),
-            endTime = endTime.toEpochSecond()
+        GoogleSignIn.requestPermissions(
+            this,
+            SESSION_PERMISSIONS_REQUEST_CODE,
+            googleAccount,
+            fitnessOptions
         )
-
-        val googleSignInAccount =
-            GoogleSignIn.getAccountForExtension(this, sessionFitnessOptions)
-
-        Fitness.getHistoryClient(this, googleSignInAccount)
-            .readData(sessionReadRequest)
-            .addOnSuccessListener { response ->
-                val flatMap = response.buckets
-                    .flatMap { it.dataSets }
-                    .flatMap { it.dataPoints }
-                val totalSteps = flatMap
-                    .sumBy { it.getValue(Field.FIELD_STEPS).asInt() }
-                print("")
-            }
     }
 
-    private fun sendSessionsRequestAndGetResponse() {
+    private fun getSessionHeartRateAverage() {
         val dataTypes =
             arrayListOf(DataType.TYPE_HEART_RATE_BPM)
 
-        val sessionFitnessOptions = getSessionFitnessOptions(dataTypes)
+        val fitnessOptions = getSessionFitnessOptions(dataTypes)
 
         val queryStartTime = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             LocalDate.now().atStartOfDay(ZoneId.systemDefault()).minusDays(10)
@@ -117,46 +74,69 @@ class MainActivity : AppCompatActivity() {
         }
         val endTime = LocalDateTime.now().atZone(ZoneId.systemDefault())
 
-        val sessionReadRequest = getSessionReadRequest(
-            dataTypes = dataTypes,
+        val sessionReadRequest = getSessionHeartRateReadRequest(
             startTime = queryStartTime.toEpochSecond(),
             endTime = endTime.toEpochSecond()
         )
 
-        val googleSignInAccount =
-            GoogleSignIn.getAccountForExtension(this, sessionFitnessOptions)
+        val googleAccount = GoogleSignIn.getAccountForExtension(this, fitnessOptions)
 
-        Fitness.getSessionsClient(this, googleSignInAccount)
+        Fitness.getSessionsClient(this, googleAccount)
             .readSession(sessionReadRequest)
             .addOnSuccessListener { response ->
                 for (session in response.sessions) {
-                    val sessionStart = session.getStartTime(TimeUnit.MILLISECONDS)
-                    val sessionEnd = session.getEndTime(TimeUnit.MILLISECONDS)
-                    Log.i("TAG", "za between $sessionStart and $sessionEnd")
-
                     val dataSets = response.getDataSet(session)
                     for (dataSet in dataSets) {
-                        val firstDataPoint = dataSet.dataPoints[0]
-                        var initialTimestamp = firstDataPoint.getStartTime(TimeUnit.MILLISECONDS)
-                        var initialHeartRate = firstDataPoint.getValue(Field.FIELD_BPM).asFloat()
+                        val dataPoints = dataSet.dataPoints
 
-                        val intervals = dataSet.dataPoints.subList(1, dataSet.dataPoints.size).map {
-                            val startTime = it.getStartTime(TimeUnit.MILLISECONDS)
-                            val heartRate = it.getValue(Field.FIELD_BPM).asFloat()
-                            val pair = Pair(
-                                first = startTime - initialTimestamp,
-                                second = (heartRate + initialHeartRate) / 2,
-                            )
-                            initialTimestamp = startTime
-                            initialHeartRate = heartRate
-                            pair
+                        dataPoints.firstOrNull()?.let { firstDataPoint ->
+                            var initialTimestamp =
+                                firstDataPoint.getStartTime(TimeUnit.MILLISECONDS)
+                            var initialHeartRate =
+                                firstDataPoint.getValue(Field.FIELD_BPM).asFloat()
+
+                            if (dataPoints.size > 1) {
+                                val intervals = dataPoints.subList(1, dataPoints.size).map {
+                                    val startTime = it.getStartTime(TimeUnit.MILLISECONDS)
+                                    val heartRate = it.getValue(Field.FIELD_BPM).asFloat()
+                                    val pair = Pair(
+                                        first = startTime - initialTimestamp,
+                                        second = (heartRate + initialHeartRate) / 2,
+                                    )
+                                    initialTimestamp = startTime
+                                    initialHeartRate = heartRate
+                                    pair
+                                }
+                                //TODO return it
+                                val averageHeartRate =
+                                    intervals.sumOf { it.first.toDouble() * it.second } / intervals.sumOf { it.first }
+                            } else {
+                                //TODO return it
+                                val averageHeartRate = initialHeartRate
+                            }
                         }
-                        val average =
-                            intervals.sumOf { it.first.toDouble() * it.second } / intervals.sumOf { it.first }
-                        print("")
                     }
                 }
             }
+    }
+
+    private fun getSessionHeartRateReadRequest(
+        startTime: Long,
+        endTime: Long,
+    ): SessionReadRequest {
+//        val datasource = DataSource.Builder()
+//            .setAppPackageName("com.google.android.gms")
+//            .setDataType(DataType.TYPE_HEART_RATE_BPM)
+//            .setType(DataSource.TYPE_DERIVED)
+//            .build()
+
+        return SessionReadRequest.Builder()
+            .includeActivitySessions()
+            .enableServerQueries()
+            .readSessionsFromAllApps()
+            .read(DataType.TYPE_HEART_RATE_BPM)
+            .setTimeInterval(startTime, endTime, TimeUnit.SECONDS)
+            .build()
     }
 
     private fun getSessionFitnessOptions(dataTypes: List<DataType>): FitnessOptions {
@@ -169,36 +149,63 @@ class MainActivity : AppCompatActivity() {
         return fitnessOptionsBuilder.build()
     }
 
-    private fun getHistoryFitnessOptions(dataTypes: List<DataType>): FitnessOptions {
-        val fitnessOptionsBuilder = FitnessOptions.builder()
+    private fun getDailyStepInformation() {
+        val dataTypes = arrayListOf(DataType.TYPE_STEP_COUNT_DELTA)
+        val arePermissionsGranted = areHistoryPermissionsGranted(dataTypes)
 
-        dataTypes.forEach {
-            fitnessOptionsBuilder.addDataType(it, FitnessOptions.ACCESS_READ)
+        if (arePermissionsGranted) {
+            getDailyStepDataForTimeInterval()
+        } else {
+            requestHistoryPermissions(dataTypes)
         }
-        return fitnessOptionsBuilder.build()
     }
 
-    private fun getSessionReadRequest(
-        startTime: Long,
-        endTime: Long,
-        dataTypes: List<DataType>
-    ): SessionReadRequest {
-        val datasource = DataSource.Builder()
-            .setAppPackageName("com.google.android.gms")
-            .setDataType(DataType.TYPE_HEART_RATE_BPM)
-            .setType(DataSource.TYPE_DERIVED)
-            .build()
+    private fun areHistoryPermissionsGranted(dataTypesOfPermissions: List<DataType>): Boolean {
+        val fitnessOptions = getHistoryFitnessOptions(dataTypesOfPermissions)
+        val googleAccount = GoogleSignIn.getAccountForExtension(this, fitnessOptions)
+        return GoogleSignIn.hasPermissions(googleAccount, fitnessOptions)
+    }
 
-        val sessionReadRequestBuilder = SessionReadRequest.Builder()
-            .includeActivitySessions()
-            .enableServerQueries()
-            .readSessionsFromAllApps()
-            .read(DataType.TYPE_HEART_RATE_BPM)
-            .setTimeInterval(startTime, endTime, TimeUnit.SECONDS)
+    private fun requestHistoryPermissions(dataTypesOfPermissions: List<DataType>) {
+        val fitnessOptions = getHistoryFitnessOptions(dataTypesOfPermissions)
+        val googleAccount = GoogleSignIn.getAccountForExtension(this, fitnessOptions)
 
+        GoogleSignIn.requestPermissions(
+            this,
+            SESSION_PERMISSIONS_REQUEST_CODE,
+            googleAccount,
+            fitnessOptions
+        )
+    }
 
-        return sessionReadRequestBuilder
-            .build()
+    private fun getDailyStepDataForTimeInterval() {
+        val dataTypes = arrayListOf(DataType.TYPE_STEP_COUNT_DELTA)
+        val fitnessOptions = getHistoryFitnessOptions(dataTypes)
+
+        val startTime = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            LocalDate.now().atStartOfDay(ZoneId.systemDefault()).minusDays(10)
+        } else {
+            TODO("VERSION.SDK_INT < O")
+        }
+        val endTime = LocalDateTime.now().atZone(ZoneId.systemDefault())
+
+        val historyReadRequest = getHistoryReadRequest(
+            startTime = startTime.toEpochSecond(),
+            endTime = endTime.toEpochSecond()
+        )
+
+        val googleAccount = GoogleSignIn.getAccountForExtension(this, fitnessOptions)
+
+        Fitness.getHistoryClient(this, googleAccount)
+            .readData(historyReadRequest)
+            .addOnSuccessListener { response ->
+                val flatMap = response.buckets
+                    .flatMap { it.dataSets }
+                    .flatMap { it.dataPoints }
+                val totalSteps = flatMap
+                    .sumBy { it.getValue(Field.FIELD_STEPS).asInt() }
+                print("")
+            }
     }
 
     private fun getHistoryReadRequest(
@@ -212,19 +219,43 @@ class MainActivity : AppCompatActivity() {
             .setStreamName("estimated_steps")
             .build()
 
-        val request = DataReadRequest.Builder()
+        return DataReadRequest.Builder()
             .aggregate(datasource)
             .bucketByTime(1, TimeUnit.DAYS)
             .setTimeRange(startTime, endTime, TimeUnit.SECONDS)
             .build()
+    }
 
-        return request
+    private fun getHistoryFitnessOptions(dataTypes: List<DataType>): FitnessOptions {
+        val fitnessOptionsBuilder = FitnessOptions.builder()
+
+        dataTypes.forEach {
+            fitnessOptionsBuilder.addDataType(it, FitnessOptions.ACCESS_READ)
+        }
+        return fitnessOptionsBuilder.build()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (resultCode) {
+            Activity.RESULT_OK -> when (requestCode) {
+                SESSION_PERMISSIONS_REQUEST_CODE -> getSessionHeartRateAverage()
+                HISTORY_PERMISSIONS_REQUEST_CODE -> getDailyStepDataForTimeInterval()
+                else -> {
+                    // Result wasn't from Google Fit
+                }
+            }
+            else -> {
+                // Permission not granted
+            }
+        }
     }
 }
 
 object PermissionConstants {
-    const val GOOGLE_FIT_PERMISSIONS_REQUEST_CODE = 1111
-    const val ACTIVITY_RECOGNITION_REQUEST_CODE = 1112
+    const val SESSION_PERMISSIONS_REQUEST_CODE = 1111
+    const val HISTORY_PERMISSIONS_REQUEST_CODE = 1112
+    const val ACTIVITY_RECOGNITION_REQUEST_CODE = 1113
 }
 
 
